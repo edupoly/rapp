b6a06aacd76b02743550a6f19eb691f5220f7171
edupoly <info.edupoly@gmail.com>
2023-07-17 07:33:38 +0530

    length

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 function App() {
-  var [ar,setAr]= React.useState([10,47,54,22,45,98,43]);
-  var [t,setT] = React.useState(0);
-  function evens(){
-    setT(0)
-  }
-  function odds(){
-    setT(1)
-  }
+  var [myname,setMyname] = React.useState("praveen")
   return (
     <div>
-      <button onClick={evens}>Evens</button>
-      <button onClick={odds}>Odds</button>
-      <ul>
-        {
-          ar.map((a)=>{
-            if(a%2===t){
-              return <li>{a}</li>
-            }
-          })
-        }
-      </ul>
+      {myname.length%2===0 && (<i>{myname}</i>)}
+      {myname.length%2===1 && (<b>{myname}</b>)}
     </div>
   );
 }
