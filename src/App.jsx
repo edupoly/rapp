42cbed3a2956a3c49c7ad4a96dc6776cbf032386
edupoly <info.edupoly@gmail.com>
2023-07-24 07:42:23 +0530

    sdfsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import Counter from './Counter';
 
 function App() {
   const [a, seta] = React.useState(123)
-  var ob1 = {firstname:'ravi'};
-  React.useEffect(()=>{
-    console.log("App Component useEffect with Empty DA called")
-  },[])
-  React.useEffect(()=>{
-    console.log("App Component useEffect with no DA called")
-  })
-  function chg(){
-    seta(a*23*Math.random())
-  }
-  var fun1 = useCallback(function(){
-    console.log("Hello")
-  },[])
   
-  var ar = React.useMemo(()=>{
-    return [12,23]
-  },[])
+  React.useEffect(()=>{
+    console.log("App Component updated")
+  })
+
+  
   return (
-    <div className='mybox'>
-      <button onClick={chg}>change a</button>
+    <div className='redbox' style={{border:"10px solid blue"}}>
+      <button onClick={()=>{seta(a*20)}}>change a</button>
       <h1>App Component : {a}</h1>
-      <Counter ar={ar}></Counter>
+      <Counter></Counter>
     </div>
   );
 }
