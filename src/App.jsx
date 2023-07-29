d48757a414b6c5ddc24512d42d20b0c9ecf9a4e2
edupoly <info.edupoly@gmail.com>
2023-07-29 07:42:14 +0530

    akjsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import {useFormik} from 'formik'
+import {Formik,Field, useFormik, ErrorMessage} from 'formik'
 import * as Yup from 'yup';
 function App() {
-  var myform = useFormik({
-    initialValues:{
-      firstname:'',
-      email:''
-    },
-    validationSchema: Yup.object({
-      firstname: Yup.string().required("koncham chusukooo"),
-      email: Yup.string().email().required("sarigga enter cheyy..")
-    }),
-    onSubmit:(x)=>{
-      console.log("HI")
-      console.log(x)
-    }
-  })
+  
   return (
     <div style={{border:'2px solid blue',padding:"10px",margin:"10px"}}>
      <h1>Formik Practical Class</h1>
      <div style={{border:'2px solid green',padding:"10px",margin:"10px"}}>
-      <form onSubmit={myform.handleSubmit}>
-        <input type="text" {...myform.getFieldProps("firstname")}/>
-        <i>{myform.touched.firstname && myform.errors.firstname}</i>
-        <br />
-        <input type="email" {...myform.getFieldProps("email")}/>
-        <i>{myform.touched.email && myform.errors.email}</i>
+      <Formik initialValues={{
+                firstname:'',
+                email:''
+              }}
+              validationSchema={ Yup.object({
+                firstname: Yup.string().required("koncham chusukooo"),
+                email: Yup.string().email().required("sarigga enter cheyy..")
+              })}
+              onSubmit={(x)=>{
+                console.log("HI")
+                console.log(x)
+              }}
+      >
+        {
+          (myform)=>{
+            return (
+              <form onSubmit={myform.handleSubmit}>
+                <Field name="firstname"></Field>
+                <ErrorMessage name="firstname" component="div"></ErrorMessage>
+                <Field type="email" name="email"></Field>
+                <ErrorMessage name="email" component="b"></ErrorMessage>
+                <br />
+                <button type="submit">Save</button>
+              </form>
+            )
+          }
+        }
+      </Formik>
 
-        <br />
-        <button type="submit">Save</button>
-      </form>
+      
+
+
+
      </div>
     </div>
   );
