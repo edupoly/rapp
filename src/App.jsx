e6a69bb8f30b97d6cd85d6a277ed78405dd4d66f
edupoly <info.edupoly@gmail.com>
2023-07-29 08:07:05 +0530

    easd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import {Outlet} from 'react-router-dom'
 function App() {
   return (
     <div>
-     
+     <h1>This is App</h1>
+     <a href="/contact">Click here to Contactus</a>
+     &nbsp;&nbsp;&nbsp;
+     <a href="/aboutus">Click here know me...</a>
+     <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Contactus from './Contactus';
+import Aboutus from './Aboutus';
+var router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App></App>,
+        children:[
+            {
+                path:"/contact",
+                element:<Contactus></Contactus>
+            },
+            {
+                path:"/aboutus",
+                element:<Aboutus></Aboutus>
+            }
+        ]
+    },
+    
+])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+      <RouterProvider router={router} />
+  );
 
-    <App />
-
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
